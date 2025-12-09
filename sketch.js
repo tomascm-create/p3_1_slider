e86e92e65555543ce03f5ee34497de99ef4b8cc2
tomascm-create <tomascm@easdalcoi.es>
2025-12-09 17:47:12 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+var slider=document.getElementById("slider");
+function setup() {
+  var c = createCanvas(400, 400); 
+  c.parent("canvasWrapper");
+}
+
+function draw() {
+  background(220);
+  fill(0);
+  ellipse(width/2, height/2,slider.value, slider.value);
+}
